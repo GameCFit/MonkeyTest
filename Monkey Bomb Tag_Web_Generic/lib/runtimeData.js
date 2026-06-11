770cf149d8c9c44ff8adcc2264791832de12bf90
GameCFit <gamecrafting77@gmail.com>
2026-06-12 01:32:36 +0400

    залито

---
+++ Monkey Bomb Tag_Web_Generic/lib/runtimeData.js
@@ -0,0 +1,70 @@
+const runtimeData = (function () {
+
+    return {
+
+        // Basic information.
+        companyName: "DefaultCompany",
+        productName: "Monkey Bomb Tag",
+        productVersion: "0.1",
+        sdkVersion: "3.19.12+merge4",
+        productDescription: "",
+
+        // File references.
+        buildURL: "bin",
+        loaderURL: "bin/Monkey Bomb Tag_Web_Generic.loader.js",
+        dataURL: "bin/Monkey Bomb Tag_Web_Generic.data.unityweb",
+        frameworkURL: "bin/Monkey Bomb Tag_Web_Generic.framework.js.unityweb",
+        workerURL: "",
+        codeURL: "bin/Monkey Bomb Tag_Web_Generic.wasm.unityweb",
+        symbolsURL: "",
+        streamingURL: "streaming",
+
+        // Visual information.
+        logoType: "LOGO_TYPE",
+        iconTextureName: "game_logo_256x256.png",
+        backgroundTextureName: "background_1280x720.png",
+
+        // Aspect ratio.
+        desktopAspectRatio: -1,
+        mobileAspectRatio: -1,
+
+        // Debug mode.
+        debugMode: false,
+        rotationLockType : "None",
+
+        // Prefs.
+        prefsContainerTags: [ "json-data" ],
+
+        // Platform specific scripts.
+        wrapperScript: "prototypeWrapper.js",
+
+        // YandexGames.
+        yandexGamesSDK: "/sdk.js",
+
+        // Yandex Ads Network.
+        yandexGameId: "",
+        yandexBannerId: "",
+        yandexInterstitialDesktopId: "",
+        yandexInterstitialMobileId: "",
+        yandexRewardedDesktopId: "",
+        yandexRewardedMobileId: "",
+
+        // GameDistribution.
+        gameDistributionId: "",
+        gameDistributionPrefix: "mirragames_",
+
+        // CrazyGames.
+        crazyGamesXSollaProjectId: "",
+
+        // Ads by Google.
+        googleAdsClient: "",
+        googleAdsChannel: "",
+        googleAdsTest: true,
+
+        // GamePush.
+        gamepushProjectId: "",
+        gamepushToken: "",
+
+    }
+
+})();
